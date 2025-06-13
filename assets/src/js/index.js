2078611956301de2314b9382b6d565e6f3824238
Javier Sierra <agradecido@gmail.com>
2025-06-13 15:07:10 +0200

    chore: remove tailwind tesst

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -5,4 +5,3 @@ import '../scss/history.scss';
 // Import JS modules
 import './chatbot.js';
 import './history.js'; // Import the history module
-import './tailwind-test.js'; // Import the Tailwind test component
